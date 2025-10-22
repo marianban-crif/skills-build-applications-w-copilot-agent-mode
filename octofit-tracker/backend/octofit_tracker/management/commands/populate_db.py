7d3c28cbdceaf4fc465915020d3a60a72449d926
Marian Ban <m.ban@consultant.crif.com>
2025-10-22 08:52:04 +0000

    Refactor database models and migrations to use ObjectIdField for unique identifiers

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,32 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-from djongo import models
-
-class Team(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    class Meta:
-        app_label = 'octofit_tracker'
-
-class Activity(models.Model):
-    name = models.CharField(max_length=100)
-    user = models.CharField(max_length=100)
-    team = models.CharField(max_length=100)
-    class Meta:
-        app_label = 'octofit_tracker'
-
-class Leaderboard(models.Model):
-    user = models.CharField(max_length=100)
-    team = models.CharField(max_length=100)
-    score = models.IntegerField()
-    class Meta:
-        app_label = 'octofit_tracker'
-
-class Workout(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    user = models.CharField(max_length=100)
-    class Meta:
-        app_label = 'octofit_tracker'
+from octofit_tracker.models import Team, Activity, Leaderboard, Workout
 
 class Command(BaseCommand):
     help = 'Populate the octofit_db database with test data'
--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,12 +1,17 @@
 from djongo import models
-from django.contrib.auth.models import AbstractUser
+from bson import ObjectId
+
+# Explicit ObjectIdField primary keys with default callable to ensure ObjectIds
+# are generated at creation time and serialized as 'id'.
 
 class Team(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId)
     name = models.CharField(max_length=100, unique=True)
     def __str__(self):
         return self.name
 
 class Activity(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId)
     name = models.CharField(max_length=100)
     user = models.CharField(max_length=100)
     team = models.CharField(max_length=100)
@@ -14,6 +19,7 @@ class Activity(models.Model):
         return f"{self.name} - {self.user}"
 
 class Leaderboard(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId)
     user = models.CharField(max_length=100)
     team = models.CharField(max_length=100)
     score = models.IntegerField()
@@ -21,6 +27,7 @@ class Leaderboard(models.Model):
         return f"{self.user} - {self.score}"
 
 class Workout(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId)
     name = models.CharField(max_length=100)
     description = models.TextField()
     user = models.CharField(max_length=100)
